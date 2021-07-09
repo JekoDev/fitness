9456fc7bdace789410e340217d93acc8b418666b
patwerk <p.handwerk@gmx.de>
2021-07-09 13:58:25 +0200

    UI Main Menu Navigation & Animation

---
+++ Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo
+{
+    #region Properties
+    private string _username;
+
+    public string Username
+    {
+        get { return _username; }
+        set { _username = value; }
+    }
+
+    private int _points;
+
+    public int Points
+    {
+        get { return _points; }
+        set { _points = value; }
+    }
+
+    private Dictionary<Challenge, ChallengeInfo> _progress;
+
+    public Dictionary<Challenge, ChallengeInfo> Progress
+    {
+        get { return _progress; }
+        set { _progress = value; }
+    }
+
+    #endregion
+
+    public PlayerInfo()
+    {
+        _progress = new Dictionary<Challenge, ChallengeInfo>();
+        _progress[Challenge.Yoga] = new ChallengeInfo(Challenge.Yoga);
+        _progress[Challenge.Yoga] = new ChallengeInfo(Challenge.Boxing);
+        _progress[Challenge.Yoga] = new ChallengeInfo(Challenge.Running);
+    }
+
+
+    public void UpdateChallengeInfo(Challenge c, int points)
+    {
+       _progress[c].ChallengePoints = points;
+    }
+}
---
+++ Assets/Scripts/UIScripts/MenuNavigation.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuNavigation : MonoBehaviour
+{
+    public GameObject currentPanel;
+    public GameObject lastPanel;
+    public GameObject backButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < currentPanel.transform.parent.gameObject.transform.childCount; i++)
+        {
+            currentPanel.transform.parent.gameObject.transform.GetChild(i).gameObject.SetActive(false);
+        }
+
+        currentPanel.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GotoMenu(GameObject PanelToShow)
+    {
+        lastPanel = currentPanel;
+        PanelToShow.transform.position = new Vector3(Screen.width * 2, PanelToShow.transform.position.y, PanelToShow.transform.position.z);
+        //currentPanel.SetActive(false);
+        
+
+        LeanTween.moveX(currentPanel, currentPanel.transform.position.x - Screen.width, .35f);
+
+        PanelToShow.SetActive(true);
+        currentPanel = PanelToShow;
+
+        LeanTween.moveX(PanelToShow, Screen.width / 2, .7f);
+        ShowHideBackButton(true);
+    }
+
+    public void ShowHideBackButton(bool show)
+    {
+        if (show)
+        {
+            backButton.SetActive(true);
+            LeanTween.moveX(backButton, backButton.transform.position.x - (Screen.width * 0.1f), .7f);
+        }
+        else
+            LeanTween.moveX(backButton, backButton.transform.position.x + (Screen.width * 0.1f), .7f);
+
+    }
+
+    public void GoBack()
+    {
+        LeanTween.moveX(currentPanel, currentPanel.transform.position.x - Screen.width, .35f);
+
+        lastPanel.SetActive(true);
+        currentPanel = lastPanel;
+
+        LeanTween.moveX(lastPanel, Screen.width / 2, .7f);
+        ShowHideBackButton(false);
+        lastPanel = null;
+    }
+
+
+    public void QuitGame()
+    {
+
+#if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
+    }
+}
