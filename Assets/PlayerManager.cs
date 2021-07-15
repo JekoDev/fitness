ecede417c7ffa5c0bdc4d08247c389b026b96c6c
patwerk <p.handwerk@gmx.de>
2021-07-15 15:59:40 +0200

    Player Register / Save / Load

---
+++ Assets/PlayerManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class PlayerManager
+{
+    #region Properties
+    private string storagepath = Application.persistentDataPath + "/player.sav";
+
+    private PlayerInfo _player;
+
+    public PlayerInfo Player
+    {
+        get { return _player; }
+        set { _player = value; }
+    }
+
+    #endregion
+
+    #region constructors
+
+    public PlayerManager()
+    {
+
+    }
+
+    #endregion
+
+    #region Functions
+
+    #region SaveLoad PlayerData
+
+    public void SavePlayer()
+    {
+        if (_player != null)
+        {
+            BinaryFormatter storage = new BinaryFormatter();
+            using (FileStream s = new FileStream(storagepath, FileMode.Create))
+            {
+                storage.Serialize(s, _player);
+                s.Close();
+            } 
+        }
+    }
+    
+    public void CreatePlayer(string username)
+    {
+        _player = new PlayerInfo(username);
+    }
+
+    public void LoadPlayer()
+    {
+        if (PlayerExists())
+        {
+            BinaryFormatter storage = new BinaryFormatter();
+            using (FileStream s = new FileStream(storagepath, FileMode.Open))
+            {
+                _player = (PlayerInfo)storage.Deserialize(s);
+                s.Close();
+            }
+        }
+    }
+
+
+
+    public bool PlayerExists()
+    {
+        return File.Exists(Application.persistentDataPath + "/player.sav");
+    }
+
+    #endregion
+
+    #region Settings
+
+    public void SetMusicVolume(float value)
+    {
+        _player.MusicVolume = value;
+        Debug.Log(_player.MusicVolume);
+
+    }
+
+    public void SetSFXVolume(float value)
+    {
+        _player.SFXVolume = value;
+        Debug.Log(_player.SFXVolume);
+    }
+
+    public void SetLanguage(Language l)
+    {
+        _player.Langauge = (int)l;
+
+        Debug.Log(_player.Langauge.ToString());
+
+    }
+
+    #endregion
+
+    #endregion
+
+    
+}
--- Assets/Scripts/PlayerInfo.cs
+++ Assets/Scripts/PlayerInfo.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class PlayerInfo
 {
     #region Properties
@@ -13,35 +14,90 @@ public class PlayerInfo
         set { _username = value; }
     }
 
-    private int _points;
+    private int _playerlevel;
 
-    public int Points
+    public int Level
     {
-        get { return _points; }
-        set { _points = value; }
+        get { return _playerlevel; }
+        set { _playerlevel = value; }
     }
 
-    private Dictionary<Challenge, ChallengeInfo> _progress;
+    private int _language;
 
-    public Dictionary<Challenge, ChallengeInfo> Progress
+    public int Langauge
     {
-        get { return _progress; }
-        set { _progress = value; }
+        get { return _language; }
+        set { _language = value; }
+    }
+
+    private float _musicVolume;
+
+    private int[] _exercisepoints;
+
+    public int[] Points
+    {
+        get { return _exercisepoints; }
+        set { _exercisepoints = value; }
+    }
+
+
+    public float MusicVolume
+    {
+        get { return _musicVolume; }
+        set { _musicVolume = value; }
+    }
+
+    private float _sfxVolume;
+
+    public float SFXVolume
+    {
+        get { return _sfxVolume; }
+        set { _sfxVolume = value; }
     }
 
     #endregion
 
+    public PlayerInfo(PlayerInfo p)
+    {
+        _username = p.Username;
+        _musicVolume = p.MusicVolume;
+        _sfxVolume = p.SFXVolume;
+        _language = p.Langauge;
+        _playerlevel = p.Level;
+        _exercisepoints = new int[3];
+        _exercisepoints[0] = p.Points[0];
+        _exercisepoints[1] = p.Points[1];
+        _exercisepoints[2] = p.Points[2];
+    }
+
     public PlayerInfo()
     {
-        _progress = new Dictionary<Challenge, ChallengeInfo>();
-        _progress[Challenge.Yoga] = new ChallengeInfo(Challenge.Yoga);
-        _progress[Challenge.Yoga] = new ChallengeInfo(Challenge.Boxing);
-        _progress[Challenge.Yoga] = new ChallengeInfo(Challenge.Running);
+        _username = "unidentified";
+        _musicVolume = 1.0f;
+        _sfxVolume = 1.0f;
+        _language = 0;
+        _playerlevel = 1;
+        _exercisepoints[0] = 0;
+        _exercisepoints[1] = 0;
+        _exercisepoints[2] = 0;
     }
 
-
-    public void UpdateChallengeInfo(Challenge c, int points)
+    public PlayerInfo(string name)
     {
-       _progress[c].ChallengePoints = points;
+        _username = name;
+        _musicVolume = 1.0f;
+        _sfxVolume = 1.0f;
+        _language = 0;
+        _playerlevel = 1;
+        _exercisepoints = new int[3];
+        _exercisepoints[0] = 0;
+        _exercisepoints[1] = 0;
+        _exercisepoints[2] = 0;
     }
+}
+
+public enum Language
+{
+    deDE = 0,
+    enEN
 }
